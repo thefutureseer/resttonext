25b801ca9783999762f966e435f1fc8bc4d748cb
thefutureseer <danielharrislimos@outlook.com>
2024-08-04 01:08:40 -0700

    first delete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
