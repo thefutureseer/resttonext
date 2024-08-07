45ee61de29cb10aed82c91414faa83175c251ad4
thefutureseer <danielharrislimos@outlook.com>
2024-08-07 06:45:10 -0700

    add tailwind form spaces

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -10,6 +10,7 @@ const SignInForm:React.FC = () => {
      </div>
 
      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
       <form className="space-y-6" action="#" method="POST">
         
        <div>
@@ -19,13 +20,15 @@ const SignInForm:React.FC = () => {
         </div>
        </div>
 
-       <div className="flex items-center justify-between">
+       <div>
         <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="password">password</label>
         <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-        </div>
-        <div className="mt-2">
          <input className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="password" name="password" type="password" autoFocus required></input>
+        </div>
+        <div className="mt-2 flex items-center justify-between">
+         <div className="text-sm">
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+         </div>
         </div>
        </div>
 
@@ -33,11 +36,12 @@ const SignInForm:React.FC = () => {
         <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">sign in</button>
        </div>
       </form>
+
       <p className="mt-10 text-center text-sm text-gray-500">
-      Not a member?
-      <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-    </p>
-     </div>
+        Not a member?
+       <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Start a 14 day free trial</a>
+      </p>
+    </div>
     </div>
   )
 };
