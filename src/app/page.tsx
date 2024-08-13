4a60a8a0f337500bbb55ca3c4d887c0f451ed261
thefutureseer <danielharrislimos@outlook.com>
2024-08-13 04:28:12 -0700

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NavBar from "@/components/NavBar";
 import SignInForm from "@/components/SignInForm";
 import "../styles/globals.css";
 
 export default function Home() {
   return (
     <div>
+      <NavBar/>
       <SignInForm/>
     </div>);
 }
