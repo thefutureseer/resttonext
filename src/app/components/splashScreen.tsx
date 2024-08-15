38b0ec4097f47068da0247867ca508d77099b574
thefutureseer <danielharrislimos@outlook.com>
2024-08-15 02:40:08 -0700

    circlular loading img

--- src/app/components/splashScreen.tsx
+++ src/app/components/splashScreen.tsx
@@ -5,14 +5,15 @@ const SplashScreen:React.FC = () => {
 
   return (
     <div className="splash-screen">
-      <Image 
+
+      <h1>      
+       <Image 
         src="/logo_2024.svg"
         alt="splash screen"
         width={80}
         height={80}
-        className="animate-spin"
-      />
-      <h1>  Loading..</h1>
+        className="rounded-full dark:invert animate-spin"
+       />  Loading..</h1>
     </div>
   );
 };
