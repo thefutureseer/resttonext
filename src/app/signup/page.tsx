fbac92056e9920b98aa0133f36f6a5068b479cec
thefutureseer <danielharrislimos@outlook.com>
2024-08-22 05:19:11 -0700

    delete use

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useEffect, use} from "react";
+import React, {useState, useEffect} from "react";
 import Image from "next/image";
 import NavBar from "../components/ui/NavBar";
 
