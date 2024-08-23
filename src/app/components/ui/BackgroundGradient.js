9c803bb1e5185737e14632bf984555139d32ae87
thefutureseer <danielharrislimos@outlook.com>
2024-08-23 02:28:43 -0700

    clean gradient

--- src/app/components/ui/BackgroundGradient.js
+++ src/app/components/ui/BackgroundGradient.js
@@ -1,11 +1,11 @@
-import React from "react";
+// import React from "react";
 
-const GradientBackground = () => {
-  return (
-    <div className="absolute inset-0 -z-10">
-      <div className="h-full w-full bg-gradient-to-br from-white via-[#f7e8a4] to-[#d4af37] opacity-80 blur-xl" />
-    </div>
-  );
-};
+// const GradientBackground = () => {
+//   return (
+//     <div className="absolute inset-0 -z-10">
+//       <div className="h-full w-full bg-gradient-to-br from-white via-[#f7e8a4] to-[#d4af37] opacity-80 blur-xl" />
+//     </div>
+//   );
+// };
 
-export default GradientBackground;
+// export default GradientBackground;
